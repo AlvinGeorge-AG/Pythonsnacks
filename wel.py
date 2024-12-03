c77b71681dbb5a6854066da4ca6ea541df09e80a
ALVIN GEORGE <Alvingeorge_@outlook.com>
2024-12-03 18:14:32 +0000

    d

--- wel.py
+++ wel.py
@@ -1,1 +1,37 @@
-print("Welcome")
+# Python program to automatically generate CAPTCHA and
+import random
+
+# Returns true if given two strings are same
+def checkCaptcha(captcha, user_captcha):
+	if captcha == user_captcha:
+		return True
+	return False
+
+# Generates a CAPTCHA of given length
+def generateCaptcha(n):
+	
+	# Characters to be included
+	chrs = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	
+	# Generate n characters from above set and
+	# add these characters to captcha.
+	captcha = ""
+	while (n):
+		captcha += chrs[random.randint(1, 63) % 62]
+		n -= 1
+	return captcha
+
+# Generate a random CAPTCHA
+captcha = generateCaptcha(6)
+print(captcha)
+
+# Ask user to enter a CAPTCHA
+print("Enter above CAPTCHA:")
+usr_captcha = input()
+
+# Notify user about matching status
+if (checkCaptcha(captcha, usr_captcha)):
+	print("CAPTCHA Matched")
+else:
+	print("CAPTCHA Not Matched")
+
